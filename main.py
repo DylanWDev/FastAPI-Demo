d112427f2a4b32aa56f4637d1d91ea1368dfc32f
Dylan Wyrick <xpoweroutage@gmail.com>
2023-11-06 19:33:47 +0000

    got read_item to gooooooo go go

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return "Hello World"
+    return {'message': 'Hello World'}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
